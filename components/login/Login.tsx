da93660811cf3d0165d785115d91cf26aacabd2b
adeolaemmanuelmorren <122842386+adeolaemmanuelmorren@users.noreply.github.com>
2023-05-18 15:16:14 -0700

    supabase starter

--- components/login/Login.tsx
+++ components/login/Login.tsx
@@ -3,93 +3,94 @@ import React, { useContext, useState } from "react";
 import { Form, Button, Col, Row } from "react-bootstrap";
 // import { useNavigate } from "react-router-dom";
 // import "./login.css";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 
-
-{/* Sign In Page Function */ }
+{
+	/* Sign In Page Function */
+}
 function Login() {
+	const supabaseClient = useSupabaseClient();
 
-  // const [login, setLogin] = useState({
-  //   username: "",
-  //   password: "",
-  // });
+	// const [login, setLogin] = useState({
+	//   username: "",
+	//   password: "",
+	// });
 
-  // let { Login } = useContext(UserContext);
-  // let { username, password } = login;
-  // let navigate = useNavigate();
+	// let { Login } = useContext(UserContext);
+	// let { username, password } = login;
+	// let navigate = useNavigate();
 
-  // function handleChange(event) {
-  //   setLogin((preValue) => {
-  //     return { ...preValue, [event.target.name]: event.target.value };
-  //   });
-  // }
+	// function handleChange(event) {
+	//   setLogin((preValue) => {
+	//     return { ...preValue, [event.target.name]: event.target.value };
+	//   });
+	// }
 
-  // function handleSubmit(event) {
-  //   event.preventDefault();
-  //   Login(username, password);
-  //   navigate(`/write`);
-  //   console.log(login);
-  // }
+	async function handleSubmit(event) {
+		event.preventDefault();
 
-  return (
-    <>
-      <div className='signinBgCover'>
-        <div style={{ minHeight: '800px' }}>
-          <div className="text-center justify-content-center align-self-center">
+		const response = await supabaseClient.auth.signInWithPassword();
 
-            {/* Sign In form section*/}
-            <div>
-              <Form id="loginCard" >
-                <Row className=" justify-content-center" id="signin-card">
+		//  use the SUpabase Login functionality here with the input parameters from the user.
 
-                  <h1 className="text-center justify-content-center align-self-center">SIGN IN</h1>
+		// navigate(`/write`);
+		console.log(login);
+	}
 
-                  <Form.Group className="mb-3">
-                    <Form.Label id="formLabelName">User Name</Form.Label>
-                    <Form.Control
-                      id="formLabel"
-                      type="username"
-                      name="username"
-                      // value={username}
-                      // onChange={handleChange}
-                    />
-                  </Form.Group>
-                </Row>
+	return (
+		<>
+			<div className="signinBgCover">
+				<div style={{ minHeight: "800px" }}>
+					<div className="text-center justify-content-center align-self-center">
+						{/* Sign In form section*/}
+						<div>
+							<Form id="loginCard">
+								<Row className=" justify-content-center" id="signin-card">
+									<h1 className="text-center justify-content-center align-self-center">
+										SIGN IN
+									</h1>
 
-                <Row className=" justify-content-center" id="signin-card">
-                  <Form.Group className="mb-3">
-                    <Form.Label id="formLabelName" >Password</Form.Label>
-                    <Form.Control
-                      id="formLabel"
-                      type="password"
-                      name="password"
-                      // value={password}
-                      // onChange={handleChange}
-                    />
-                  </Form.Group>
-                </Row>
+									<Form.Group className="mb-3">
+										<Form.Label id="formLabelName">User Name</Form.Label>
+										<Form.Control
+											id="formLabel"
+											type="username"
+											name="username"
+											// value={username}
+											// onChange={handleChange}
+										/>
+									</Form.Group>
+								</Row>
 
-                <Row className="justify-content-center" id="signin-card">
-                  <Form.Group className="col-1">
-                    <Col xs={12} sm={4} md={4} lg={4}>
-                      <Button id="signinFormButton" type="submit">
-                        SUBMIT
-                      </Button>
-                    </Col>
-                  </Form.Group>
-                </Row>
-              </Form>
-            </div>
-          </div>
-        </div>
-      </div>
+								<Row className=" justify-content-center" id="signin-card">
+									<Form.Group className="mb-3">
+										<Form.Label id="formLabelName">Password</Form.Label>
+										<Form.Control
+											id="formLabel"
+											type="password"
+											name="password"
+											// value={password}
+											// onChange={handleChange}
+										/>
+									</Form.Group>
+								</Row>
 
-
-
-
-
-
-    </>
-  );
+								<Row className="justify-content-center" id="signin-card">
+									<Form.Group className="col-1">
+										<Col xs={12} sm={4} md={4} lg={4}>
+											<Button id="signinFormButton" type="submit">
+												SUBMIT
+											</Button>
+										</Col>
+									</Form.Group>
+								</Row>
+							</Form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Login;
--- components/topBar/Topbar.tsx
+++ components/topBar/Topbar.tsx
@@ -1,10 +1,16 @@
-import Link from 'next/link';
+import Link from "next/link";
 import { useContext, useState, useEffect } from "react";
 // import UserContext from "../../context/UserContext";
-import Login from "../../pages/login/login/Login";
-import { Button, Form, Row, Col, Navbar, NavDropdown, Nav } from "react-bootstrap";
-
-
+// import Login from "../../pages/login/login/Login";
+import {
+	Button,
+	Form,
+	Row,
+	Col,
+	Navbar,
+	NavDropdown,
+	Nav,
+} from "react-bootstrap";
 
 // Documentations for the functions used for this page
 // useContext Hook: https://reactjs.org/docs/hooks-reference.html#usecontext
@@ -17,54 +23,53 @@ import { Button, Form, Row, Col, Navbar, NavDropdown, Nav } from "react-bootstra
 // Responsive breakpoints in React-Bootstrap: https://react-bootstrap.github.io/layout/grid/#grid-props
 
 export default function Topbar() {
-//   let { getUserById } = useContext(UserContext);
-//   let params = useParams();
-//   let navigate = useNavigate()
+	//   let { getUserById } = useContext(UserContext);
+	//   let params = useParams();
+	//   let navigate = useNavigate()
 
-//   const [user, setUser] = useState({
-//     userId: 0,
-//     username: "",
-//     email: "",
-//     firstName: "",
-//     lastName: "",
-//     createdAt: "",
-//     updatedAt: "",
-//   });
+	//   const [user, setUser] = useState({
+	//     userId: 0,
+	//     username: "",
+	//     email: "",
+	//     firstName: "",
+	//     lastName: "",
+	//     createdAt: "",
+	//     updatedAt: "",
+	//   });
 
-//   const [expanded, setExpanded] = useState(false);
+	//   const [expanded, setExpanded] = useState(false);
 
-//   const handleToggle = () => {
-//     setExpanded(!expanded);
-//   };
+	//   const handleToggle = () => {
+	//     setExpanded(!expanded);
+	//   };
 
+	//   useEffect(() => {
+	//     async function fetch() {
+	//       await getUserById(1).then((user) =>
+	//         setUser(user)
+	//       );
+	//       console.log(user);
+	//     }
+	//     fetch();
+	//   }, [Login]);
 
-//   useEffect(() => {
-//     async function fetch() {
-//       await getUserById(1).then((user) =>
-//         setUser(user)
-//       );
-//       console.log(user);
-//     }
-//     fetch();
-//   }, [Login]);
+	//   function logout() {
+	//     localStorage.clear();
+	//     console.log("clicked")
+	//   }
 
-//   function logout() {
-//     localStorage.clear();
-//     console.log("clicked")
-//   }
+	//   const handleChange = (event) => {
+	//     if (event.target.value === "") return;
+	//     navigate('Search/' + event.target.value)
+	//   }
 
-//   const handleChange = (event) => {
-//     if (event.target.value === "") return;
-//     navigate('Search/' + event.target.value)
-//   }
+	//   let Auth = localStorage.getItem("username");
 
-//   let Auth = localStorage.getItem("username");
+	//   function TopBar() {
+	//     // if (Auth === user.username) {
+	//       return (
 
-//   function TopBar() {
-//     // if (Auth === user.username) {
-//       return (
-      
-          {/* <Navbar id="top" expand="lg">
+	/* <Navbar id="top" expand="lg">
             <Navbar.Brand href="#home">
               <div className="topLeft">
                 <Link to="/" class="navbar-brand align-items-center">
@@ -121,82 +126,72 @@ export default function Topbar() {
         </>
       )
     }
-    else { */}
-      return (
-        <>
-       <Navbar id="top" expand="lg">
-  <Navbar.Brand href="#home">
-    <div className="topLeft">
-      <Link href="/" className="navbar-brand align-items-center">
-        <img
-          src= "/Images/logofinal-navbar.png"
-          height="120"
-          alt="Evince"
-        />
-      </Link>
-    </div>
-  </Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" id="hamburger" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto smallNav">
-      <Nav.Link className="link"  href="/">
-        HOME
-      </Nav.Link>
-      <Nav.Link className="link"  href="/Login">
-        LOGIN
-      </Nav.Link>
-      <Nav.Link className="link" href="about">
-        ABOUT
-      </Nav.Link>
-      <Nav.Link className="link"  href="bookstore">
-        BOOK STORE
-      </Nav.Link>
-    </Nav>
-    <NavDropdown id="basic-nav-dropdown" title="CONTACT">
-      <NavDropdown.Item id="dropdownContact">
-        345lemaire@gmail.com
-      </NavDropdown.Item>
-    </NavDropdown>
-    <div className="topRight">
-      <div id="searchBar">
-        <Row>
-          <Col>
-            <Form className="d-flex">
-              <Form.Control
-                id="searchInput"
-                type="search"
-                placeholder="Search For A Blog"
-                className="d-sm-flex"
-                aria-label="Search"
-                //   onChange={handleChange}
-              />
-              <Button id="searchButton" variant="outline-success">
-                <img
-                  src='Images/searchicon.png'
-                  height="20px"
-                  alt="DanTheMan"
-                />
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </div>
-    </div>
-  </Navbar.Collapse>
-</Navbar>
+    else { */
 
+	return (
+		<>
+			<Navbar id="top" expand="lg">
+				<Navbar.Brand className="navbar-brand align-items-center" href="/">
+					<div className="topLeft">
+						<img src="/Images/logofinal-navbar.png" height="120" alt="Evince" />
+					</div>
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" id="hamburger" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="mr-auto smallNav">
+						<Nav.Link className="link" href="/">
+							HOME
+						</Nav.Link>
+						<Nav.Link className="link" href="/Login">
+							LOGIN
+						</Nav.Link>
+						<Nav.Link className="link" href="about">
+							ABOUT
+						</Nav.Link>
+						<Nav.Link className="link" href="bookstore">
+							BOOK STORE
+						</Nav.Link>
+					</Nav>
+					<NavDropdown id="basic-nav-dropdown" title="CONTACT">
+						<NavDropdown.Item id="dropdownContact">
+							345lemaire@gmail.com
+						</NavDropdown.Item>
+					</NavDropdown>
+					<div className="topRight">
+						<div id="searchBar">
+							<Row>
+								<Col>
+									<Form className="d-flex">
+										<Form.Control
+											id="searchInput"
+											type="search"
+											placeholder="Search For A Blog"
+											className="d-sm-flex"
+											aria-label="Search"
+											//   onChange={handleChange}
+										/>
+										<Button id="searchButton" variant="outline-success">
+											<img
+												src="Images/searchicon.png"
+												height="20px"
+												alt="DanTheMan"
+											/>
+										</Button>
+									</Form>
+								</Col>
+							</Row>
+						</div>
+					</div>
+				</Navbar.Collapse>
+			</Navbar>
+		</>
+	);
+}
 
-</>
-
-
-      )
-    }
-  
-
-
-{/* //   return (
+{
+	/* //   return (
 //     <>
 //       {displayUser()}
 //     </>
-//   ) */}
-
+//   ) */
+}
