e4fae0ae0d9fc1bb72858660bd48ab32f5912fb9
adeolaemmanuelmorren <122842386+adeolaemmanuelmorren@users.noreply.github.com>
2023-05-26 14:52:55 -0700

    adeola's latest instructions

--- pages/api/posts/create.ts
+++ pages/api/posts/create.ts
@@ -1,13 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
+import { createClient } from "@supabase/supabase-js";
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(
+	process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+);
+
 type Data = {
 	name: string;
 };
 
-export default function handler(
+export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<Data>
 ) {
-	res.status(200).json({ name: "John Doe" });
+	const { error } = await supabase.from("posts").insert({
+		title: "New Post",
+		post_body: "......",
+		user_id: "334455",
+	});
+
+	console.log(req.body);
+	res.status(200).json({ name: "ADEOLA MORREN" });
 }
